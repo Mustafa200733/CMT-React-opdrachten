360418b2aab7b208971ce9871ec39d52c868940c
9024454 <9024454@student.zadkine.nl>
2025-10-08 12:18:06 +0200

    opdradcht1 books

---
+++ opdracht-01-books/src/components/Book.jsx
@@ -0,0 +1,13 @@
+const Book = ({image, title, author}) => {
+  return (
+    <section className="book-container">
+    <img className="image" src={image} alt="" />
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+    </section>
+    );
+}
+ 
+export default Book;
+      
+
---
+++ opdracht-01-books/src/components/BookList.jsx
@@ -0,0 +1,15 @@
+import Book from './Book';
+const BookList = () => {
+  return (
+    <section className='container'>
+
+
+      <Book title="Harry potter And The Sorcerers stone" author="Joanne Rowling"  image="./images/book-1.png"/>
+      <Book title="Geronimo Stilton Fantasia 4" author="Elisabetta Dami" image="./images/book-2.png"/>
+      <Book title="The Hunger Games " author="Suzanne Collin" image="./images/book-3.png"/>
+
+
+    </section>
+  );
+};
+export default BookList;
