1eefb7c3fccca09d73098f051e3b11c4e33ee53a
9024454 <9024454@student.zadkine.nl>
2025-10-10 18:44:24 +0200

    1.1

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import BookList from './components/BookList'; 
 import Header from './components/Header';
+import Layout from './components/layout';
+
 function App() {
 
   return (
-    <>
     
+      <Layout>
   <Header/>
     <BookList/>
-    </>
+    </Layout>
+    
   )
 }
 
