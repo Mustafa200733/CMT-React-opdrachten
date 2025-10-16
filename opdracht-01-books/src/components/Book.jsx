e1ead98b6a6bc8a6ad510b31f70fd8ea006b0ea2
9024454 <9024454@student.zadkine.nl>
2025-10-16 14:53:11 +0200

    1.3 update

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 
-const Book = ({img, title, author, verhoogTeller}) => {
+const Book = ({ img, title, author }) => {
   const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
- 
-function verhoogTeller(){
-  setAantalKeerGelezen((vorigWaarde) => vorigWaarde + 1);
-}
- 
-function verlaagTeller(){
-  setAantalKeerGelezen((vorigWaarde) => Math.max(0, vorigWaarde - 1 ));
-}
-  return (
-    <section class="book-container">
-    <img class="image" src={img} alt={title} />
-    <h2>{title}</h2>
-      <button onClick={verhoogTeller}>Keer gelezen: {aantalKeerGelezen}</button>
-      <button onClick={verlaagTeller}>verlaagt: {aantalKeerGelezen}</button>
-        <h3>{author}</h3>
-    </section>
+
+  function verhoogTeller() {
+    setAantalKeerGelezen(aantalKeerGelezen + 1);
+  }
+
+  function verlaagTeller() {
+    setAantalKeerGelezen(
+      aantalKeerGelezen > 0 ? aantalKeerGelezen - 1 : 0
     );
-}
- 
+  }
+
+  return (
+    <section className="book-container">
+      <img className="image" src={img} alt={title} />
+      <h2>{title}</h2>
+      <button onClick={verhoogTeller}>
+        Keer gelezen: {aantalKeerGelezen}
+      </button>
+      <button onClick={verlaagTeller}>
+        Verlaag: {aantalKeerGelezen}
+      </button>
+      <h3>{author}</h3>
+    </section>
+  );
+};
+
 export default Book;
-      
+
 
