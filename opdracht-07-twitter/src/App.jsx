bcc94ddc7c3a358724b2c0f260f125300bb7983c
9024454 <9024454@student.zadkine.nl>
2025-11-16 18:28:18 +0100

    Opdracht-07-twitter

--- opdracht-07-twitter/src/App.jsx
+++ opdracht-07-twitter/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import CreateMessage from './components/CreateMessage'
+import MessageList from './components/MessageList'
 
 function App() {
 
   return (
     <>
-      <h1>Opdracht 7 - Twitter</h1>
+    <CreateMessage/>
+ <MessageList/>
     </>
   )
 }
