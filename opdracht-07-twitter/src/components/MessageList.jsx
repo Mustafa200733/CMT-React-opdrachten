02f3ed64898c81784f74cedb1120a1883426fa60
9024454 <9024454@student.zadkine.nl>
2025-11-24 19:07:09 +0100

    Opdracht-07-twitter  last update

--- opdracht-07-twitter/src/components/MessageList.jsx
+++ opdracht-07-twitter/src/components/MessageList.jsx
@@ -3,15 +3,15 @@ import Message from "./Message";
 const MessageList = ({name, message}) => {
     return ( 
         <>
+          {message.map(( message) => (
     <Message name={name} message={message}/>
-    <Message name={name} message={message}/>
-    <Message name={name} message={message}/>
-    <Message name={name} message={message}/>
+    
 
 
-      
+          ))}
        </>
      );
 }
  
 export default MessageList;
+
