ae709031c3cd40591f0907e83bf9672e0f3f5364
9024454 <9024454@student.zadkine.nl>
2025-11-26 13:08:18 +0100

    update

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -2,11 +2,33 @@ import './App.css'
 import Nav from './components/Nav'
 function App() {
 
-  return (
-    <>
 
-    </>
+  return (
     
+    <div class="   bg-white dark:bg-gray-800 rounded-lg px-15 py-8 ring shadow-xl ring-gray-900/5">
+  <article> <img  class="float-left ..."   src="huisje.png" alt="" /> 
+<br /> 
+<h1 className='text-black-900   text-base font-medium tracking-tight '  >Workcation</h1> 
+  </article>
+  <br />
+  <br />
+  <br />
+  <img   src="/public/Schermafbeelding 2025-11-25 191733.png" alt="" />
+  
+  <h1 class="  text-black-900  mt-10 text-base font-medium tracking-tight ">You can work from <br />
+   anywhere. Take <br />
+   advantage of it.</h1>
+  <p class=" text-black-500 dark:text-gray-400 mt-2 text-sm ">
+    Workcation helps you find work-friendly <br />
+    rentals in beautifl locations so you can en <br />
+    joy some nice weather even when you're  <br />
+    nog on vacation.
+  </p>
+  <br />
+    <button className="text-white inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
+     &nbsp; BOOK YOUR ESCAPE &nbsp;
+    </button>
+</div>
   )
 }
 
